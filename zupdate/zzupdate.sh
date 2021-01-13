45e8b38251be44fc9e5c481a87ad2d7f16f79bb5
casitadelterror <69657855+casitadelterror@users.noreply.github.com>
2021-01-13 15:12:44 -0700

    Add files via upload

--- zupdate/zzupdate.sh
+++ zupdate/zzupdate.sh
@@ -194,7 +194,7 @@ printTitle "Tiempo que tomó Actulizacion de Repositorios de UBUNTU"
 echo "$((($(date +%s)-$TIME_START)/60)) min."
 msg -bar2
 echo -e "\033[93m         -- ACTULIZACION DE UBUNTU COMPLETA -- "
-wget https://raw.githubusercontent.com/casitadelterror/scripts/master/instalador/VPS-MX.sh -O /usr/bin/vps-mx &> /dev/null
+wget https://raw.githubusercontent.com/casitadelterror/rex/master/instalador/VPS-MX.sh -O /usr/bin/vps-mx &> /dev/null
 chmod +x /usr/bin/vps-mx
 if [ "$REBOOT" = "1" ]; then
 	printTitle "        SU VPS SE REINICIARA EN 20 SEGUNDOS           "
