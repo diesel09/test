f207faac43f7d31341b5d57b0c3809c3d6d99e62
casitadelterror <69657855+casitadelterror@users.noreply.github.com>
2021-01-13 17:42:55 -0700

    Update VPS-MX.sh

--- instalador/VPS-MX.sh
+++ instalador/VPS-MX.sh
@@ -178,7 +178,7 @@ MIP2=$(wget -qO- ipv4.icanhazip.com)
 }  
 function_verify () {
   #permitedSCRIPT
-  v1=$(curl -sSL "https://raw.githubusercontent.com/casitadelterror/scripts/master/Vercion")
+  v1=$(curl -sSL "https://raw.githubusercontent.com/casitadelterror/scripts/master/vercion")
   echo "$v1" > /etc/versin_script
  
 }
@@ -201,7 +201,7 @@ wget -O /bin/rebootnb https://raw.githubusercontent.com/VPS-MX/VPS-MX-8.0/master
 chmod +x /bin/rebootnb 
 wget -O /bin/resetsshdrop https://raw.githubusercontent.com/VPS-MX/VPS-MX-8.0/master/ArchivosUtilitarios/resetsshdrop &> /dev/null
 chmod +x /bin/resetsshdrop
-wget -O /etc/versin_script_new https://raw.githubusercontent.com/VPS-MX/VPS-MX-8.0/master/Vercion &>/dev/null
+wget -O /etc/versin_script_new https://raw.githubusercontent.com/casitadelterror/scripts/master/vercion &>/dev/null
 msg -bar2
 echo '#!/bin/sh -e' > /etc/rc.local
 sudo chmod +x /etc/rc.local
@@ -223,7 +223,7 @@ echo 'echo "" '>> .bashrc
 echo 'echo -e "\033[92m        RESELLER : $mess1 "'>> .bashrc
 echo 'echo "" '>> .bashrc                                               
 echo 'echo -e "\033[97m   PARA MOSTAR PANEL BASH ESCRIBA:  menu "'>> .bashrc
-echo 'wget -O /etc/versin_script_new https://raw.githubusercontent.com//casitadelterror/scripts/master/Vercion &>/dev/null'>> .bashrc
+echo 'wget -O /etc/versin_script_new https://raw.githubusercontent.com/casitadelterror/scripts/master/vercion &>/dev/null'>> .bashrc
 echo 'echo ""'>> .bashrc
 echo -e "         COMANDO PRINCIPAL PARA ENTRAR AL PANEL "
 echo -e "\033[1;41m                     menu                        \033[0;37m" && msg -bar2
