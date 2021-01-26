9de0fac3c5baa5c4b40d6df4c8db636a5da4e511
SCRIPTS 2021 <69657855+lacasitamx@users.noreply.github.com>
2021-01-26 08:43:48 -0700

    Add files via upload

--- instalador/VPS-MX.sh
+++ instalador/VPS-MX.sh
@@ -221,6 +221,7 @@ echo 'echo "" '>> .bashrc
 echo 'mess1="$(less /etc/newadm/message.txt)" ' >> .bashrc
 echo 'echo "" '>> .bashrc
 echo 'echo -e "\033[92m        RESELLER : $mess1 "'>> .bashrc
+echo 'echo -e "	\e[43mVERSION\e[0m : \e[1;31m$(cat /etc/versin_script_new) "'>> .bashrc
 echo 'echo "" '>> .bashrc                                               
 echo 'echo -e "\033[97m   PARA MOSTAR PANEL BASH ESCRIBA:  menu "'>> .bashrc
 echo 'wget -O /etc/versin_script_new https://raw.githubusercontent.com/lacasitamx/scripts/master/vercion &>/dev/null'>> .bashrc
