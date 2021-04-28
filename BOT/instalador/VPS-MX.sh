82507c151c8e9b54a341a46d33ba6b36ee358128
SCRIPTS 2021 <69657855+lacasitamx@users.noreply.github.com>
2021-04-27 18:24:31 -0600

    Update VPS-MX.sh

--- BOT/instalador/VPS-MX.sh
+++ BOT/instalador/VPS-MX.sh
@@ -7,6 +7,7 @@ SCPidioma="${SCPdir}/idioma"
 SCPusr="${SCPdir}/ger-user"
 SCPfrm="/etc/ger-frm"
 SCPinst="/etc/ger-inst"
+SCPv="${SCPdir}/v2ray"
 
 myip=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
 myint=`ifconfig | grep -B1 "inet addr:$myip" | head -n1 | awk '{print $1}'`;
@@ -263,6 +264,7 @@ verificar_arq () {
 [[ ! -d ${SCPusr} ]] && mkdir ${SCPusr}
 [[ ! -d ${SCPfrm} ]] && mkdir ${SCPfrm}
 [[ ! -d ${SCPinst} ]] && mkdir ${SCPinst}
+[[ ! -d ${SCPv} ]] && mkdir ${SCPv}
 case $1 in
 "menu"|"message.txt"|"slowdns.sh")ARQ="${SCPdir}/";; #Menu
 "usercodes"|"verifica")ARQ="${SCPusr}/";; #Panel SSRR
